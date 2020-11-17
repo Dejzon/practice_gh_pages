754305945083cc13752f3667e76549f0634d8dc7
Dejan <dejanpantelic1008@gmail.com>
2020-11-16 18:44:41 -0600

    Updates

---
+++ precache-manifest.4e72c025d3f12479970893877926ff1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94487a1252bd92f079a6",
+    "url": "/practice_gh_pages/static/css/main.b1ecc281.chunk.css"
+  },
+  {
+    "revision": "94487a1252bd92f079a6",
+    "url": "/practice_gh_pages/static/js/main.94487a12.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/practice_gh_pages/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "56b1cc9f847baa546661",
+    "url": "/practice_gh_pages/static/js/runtime~main.56b1cc9f.js"
+  },
+  {
+    "revision": "08246bac40b910da74407134e4e5a99a",
+    "url": "/practice_gh_pages/index.html"
+  }
+];
